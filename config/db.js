c04c191bf383f405eb59797188843b1828dcb1b5
enigma-dy <oluwagbengaadedire@gmail.com>
2025-02-06 18:51:29 +0100

    update

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_DB);
     await mongoose.connect(process.env.MONGO_DB);
     console.log("DB connected successfully");
   } catch (error) {
